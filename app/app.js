2809904a4ccbf2d555e38440ffd3c34f900bcefc
Suved Shankar <suvedshankar@gmail.com>
2019-09-16 17:15:45 +1200

    Implemented use of .env file for environment variables

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const indexRouter = require("../app/routes/index.route");
+const index = require("../app/routes/index.route");
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT;
 
 app.set("views", "app/views");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(indexRouter);
+app.use(index);
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
